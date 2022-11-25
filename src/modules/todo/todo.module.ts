c6963f0bf711967b3139d5308c0464cd3fd093df
rupakkarki27 <rupakkarki123@gmail.com>
2022-11-25 18:50:56 +0545

    feat/todo [New]: Todo Controllers and Services

--- src/modules/todo/todo.module.ts
+++ src/modules/todo/todo.module.ts
@@ -3,9 +3,16 @@ import { TodoGroupController } from './todo-group.controller';
 import { todoGroupProvider } from './todo-group.provider';
 import { TodoGroupService } from './todo-group.service';
 import { TodoController } from './todo.controller';
+import { todoProvider } from './todo.provider';
+import { TodoService } from './todo.service';
 
 @Module({
   controllers: [TodoController, TodoGroupController],
-  providers: [TodoGroupService, ...todoGroupProvider],
+  providers: [
+    TodoGroupService,
+    TodoService,
+    ...todoGroupProvider,
+    ...todoProvider,
+  ],
 })
 export class TodoModule {}
---
+++ src/modules/todo/todo.provider.ts
@@ -0,0 +1,9 @@
+import { TODO_REPOSITORY } from 'src/constants/repositories';
+import { Todo } from './entities/todo.entity';
+
+export const todoProvider = [
+  {
+    provide: TODO_REPOSITORY,
+    useValue: Todo,
+  },
+];
